51d1124ee02a2fb938f1e2a16b84a9f9affafbff
LinaChasova <a.chasova@innopolis.ru>
2019-01-04 21:53:48 +0300

    convert to MVC

---
+++ SeeFood/Controller/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Алина Часова on 04/01/2019.
+//  Copyright © 2019 LinaChasova. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
